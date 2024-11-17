39e4018ed652e77de4f751e238683b12090aa114
Syon <shen.shang.lsf@gmail.com>
2024-11-16 18:07:33 -0800

    Update autorun4music.bat

--- autorun4music.bat
+++ autorun4music.bat
@@ -1,4 +1,8 @@
 @echo off
+:: 从剪贴板获取URL并存储到变量url
 for /f "tokens=* usebackq" %%A in (`powershell -command "Add-Type -AssemblyName PresentationCore; [System.Windows.Clipboard]::GetText()"`) do set url=%%A
-powershell -Command "& { .\yt-dlp.exe -f bestaudio -x --audio-format mp3 '%url%' }"
+
+:: 使用yt-dlp下载最高品质音频并转换为MP3格式
+powershell -Command "& { .\yt-dlp.exe -f bestaudio --audio-quality 320k -x --audio-format mp3 '%url%' }"
+
 pause
